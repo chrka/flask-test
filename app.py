157775e0d2e4b35ecc506191a6c37b5fa9cc1f78
Christoffer Karlsson <chrka@mac.com>
2020-02-01 12:08:10 +0100

    Use ajax and separate endpoint

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+@app.route('/')
+def main():
+    return render_template("template.xhtml")
 
-@app.route('/', methods=['GET', 'POST'])
-def hello_world():
+@app.route('/endpoint', methods=['GET', 'POST'])
+def endpoint():
     method = 'POST' if request.method == 'POST' else 'GET'
-    return render_template("template.xhtml", method=method)
+    return render_template("endpoint.xhtml", method=method)
 
 
 if __name__ == '__main__':
